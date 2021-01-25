e9d2be4c4261d5ce58f72d81eedda37d47ad558e
BatAmar Battulga <batamar.b@nmtec.co>
2021-01-25 17:55:33 +0800

    added dd-trace

--- src/debuggers.ts
+++ src/debuggers.ts
@@ -1,3 +1,20 @@
+import tracer from "dd-trace";
+import * as formats from "dd-trace/ext/formats";
 import debug from "debug";
 
-export const debugBase = debug("erxes-message-broker:");
+export const debugBase = (message) => {
+  if (!process.env.DD_SERVICE) {
+    return debug("erxes-message-broker:")(message);
+  }
+
+  const span = tracer.scope().active();
+  const time = new Date().toISOString();
+
+  const record = { time, level: "info", message };
+
+  if (span) {
+    tracer.inject(span.context(), formats.LOG, record);
+  }
+
+  console.log(JSON.stringify(record));
+};
